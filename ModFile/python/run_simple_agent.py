fda0a28ba2fad487a313ffed5ad629efa440c805
ebe1kenobi <ebe1.kenob.i@gmail.com>
2025-02-20 20:22:08 +0100

    remove verbose python

--- ModFile/python/run_simple_agent.py
+++ ModFile/python/run_simple_agent.py
@@ -3,13 +3,13 @@ from towerfall import Towerfall
 def main():
   # Creates or reuse a Towerfall game.
   towerfall = Towerfall(
-    verbose = 1,
+    verbose = 0,
     config = dict(
       matchLengths='Quick',
       mode='HeadHunters', #Quest, DarkWorld, Trials, LastManStanding, HeadHunters, TeamDeathmatch, PlayTag( not supported = Warlord)
       level=2,
       # fps=60,
-      agentTimeout='24:00:00',      
+      agentTimeout='24:00:00',
       agents=[
         dict(type='remote', archer='white', ai='SimpleAgentLevel1'),
         dict(type='remote', archer='orange', ai='SimpleAgentLevel1'),
